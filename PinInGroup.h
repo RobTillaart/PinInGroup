59e74c5a92f86f3116e1cf96f8ca7d086f41fa43
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-05 20:55:22 +0100

    Arduino ci (#3)
    * add Arduino-CI
    * add unit test

--- PinInGroup.h
+++ PinInGroup.h
@@ -1,55 +1,59 @@
 #pragma once
 //    FILE: PinInGroup.h
 //  AUTHOR: Rob dot Tillaart at gmail dot com
-// VERSION: 0.1.2
+// VERSION: 0.1.3
 //    DATE: 2017-04-26
 // PURPOSE: PinInGroup library for Arduino
 // HISTORY: See PinInGroup.cpp
 //
 // Note: ESP32 has some dedicated IO pins that cannot be used in a group.
 //       FLASH: pin 6 - 11  (maybe more)
-//
+
 
 #include "Arduino.h"
 
-#define PININGROUP_LIB_VERSION "0.1.2"
+
+#define PININGROUP_LIB_VERSION      (F("0.1.3"))
+
 
 // smaller MAXSIZE will reduce memory footprint with ditto bytes.
 #ifndef PININGROUP_MAXSIZE
-#define PININGROUP_MAXSIZE    16
+#define PININGROUP_MAXSIZE          16
 #endif
 
 
 class PinInGroup
 {
-  public:
-    PinInGroup();
+public:
+  PinInGroup();
 
-    // enables one to reset the pinGroup and repopulate it
-    void      clear();
+  // enables one to reset the pinGroup and repopulate it
+  void      clear();
 
-    // adds a predefined array of pin numbers to the PinInGroup
-    // sets all to either INPUT or INPUT_PULLUP.
-    uint8_t    add(uint8_t sz, uint8_t * ar, uint8_t value = INPUT);
+  // adds a predefined array of pin numbers to the PinInGroup
+  // sets all to either INPUT (default) or INPUT_PULLUP.
+  uint8_t    add(uint8_t sz, uint8_t * ar, uint8_t mode = INPUT);
 
-    // adds a single pin to the PinInGroup
-    uint8_t    add(uint8_t pin, uint8_t value = INPUT);
+  // adds a single pin to the PinInGroup
+  uint8_t    add(uint8_t pin, uint8_t mode = INPUT);
 
-    // read up to 16 pins "simultaneously" in one call.
-    uint16_t  read();
+  // counts how often a pin is in the group
+  uint8_t    isInGroup(uint8_t pin);
 
-    uint8_t   size() {
-      return _size;
-    };
+  // read up to 16 pins "simultaneously" in one call.
+  uint16_t  read();
 
-    // check how many free "slots" there are...
-    uint8_t   free() {
-      return PININGROUP_MAXSIZE - _size;
-    };
+  // read specific index.
+  uint16_t  read(uint8_t idx);
 
-  private:
-    uint8_t   _pins[PININGROUP_MAXSIZE];
-    uint8_t   _size = 0;
+  uint8_t   size() { return _size; };
+
+  // check how many free "slots" there are...
+  uint8_t   free() { return PININGROUP_MAXSIZE - _size; };
+
+private:
+  uint8_t   _pins[PININGROUP_MAXSIZE];
+  uint8_t   _size = 0;
 };
 
 // -- END OF FILE --
---
+++ test/unit_test_001.cpp
@@ -0,0 +1,84 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2021-01-05
+// PURPOSE: unit tests for the PinInGroup
+//          https://github.com/RobTillaart/PinInGroup
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual);               // a == b
+// assertNotEqual(unwanted, actual);            // a != b
+// assertComparativeEquivalent(expected, actual);    // abs(a - b) == 0 or (!(a > b) && !(a < b))
+// assertComparativeNotEquivalent(unwanted, actual); // abs(a - b) > 0  or ((a > b) || (a < b))
+// assertLess(upperBound, actual);              // a < b
+// assertMore(lowerBound, actual);              // a > b
+// assertLessOrEqual(upperBound, actual);       // a <= b
+// assertMoreOrEqual(lowerBound, actual);       // a >= b
+// assertTrue(actual);
+// assertFalse(actual);
+// assertNull(actual);
+
+// // special cases for floats
+// assertEqualFloat(expected, actual, epsilon);    // fabs(a - b) <= epsilon
+// assertNotEqualFloat(unwanted, actual, epsilon); // fabs(a - b) >= epsilon
+// assertInfinity(actual);                         // isinf(a)
+// assertNotInfinity(actual);                      // !isinf(a)
+// assertNAN(arg);                                 // isnan(a)
+// assertNotNAN(arg);                              // !isnan(a)
+
+#include <ArduinoUnitTests.h>
+
+#define assertEqualFloat(arg1, arg2, arg3)  assertOp("assertEqualFloat", "expected", fabs(arg1 - arg2), compareLessOrEqual, "<=", "actual", arg3)
+// #define assertEqualINF(arg)  assertOp("assertEqualINF", "expected", INFINITY, compareEqual, "==", "actual", arg)
+// #define assertEqualNAN(arg)  assertOp("assertEqualNAN", "expected", true, compareEqual, "==", "actual", isnan(arg))
+
+
+#include "Arduino.h"
+#include "PinInGroup.h"
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+
+unittest(test_constructor)
+{
+  fprintf(stderr, "VERSION: %s\n", PININGROUP_LIB_VERSION);
+
+  PinInGroup PIG;
+  assertEqual(0, PIG.size());
+  assertEqual(16, PIG.free());
+  assertEqual(0, PIG.isInGroup(2));
+}
+
+
+unittest(test_isInGroup)
+{
+  PinInGroup PIG;
+  uint8_t ar[8] = {2, 3, 4, 5, 6, 7, 4, 4};
+
+  PIG.add(8, ar, INPUT_PULLUP);
+  assertEqual(8, PIG.size());
+  assertEqual(8, PIG.free());
+
+  assertEqual(0, PIG.isInGroup(0));
+  assertEqual(1, PIG.isInGroup(2));
+  assertEqual(1, PIG.isInGroup(3));
+  assertEqual(3, PIG.isInGroup(4));
+  assertEqual(1, PIG.isInGroup(5));
+  assertEqual(1, PIG.isInGroup(6));
+  assertEqual(1, PIG.isInGroup(7));
+  assertEqual(0, PIG.isInGroup(8));
+}
+
+unittest_main()
+
+// --------
